b19961fa5c640a4d78dd2f28dcd68be5d2d21fbe
Joaquin Sellanes <80859121+Junker6G@users.noreply.github.com>
2023-05-31 11:53:17 -0300

    a

---
+++ Pa/Lab/EMPLEADO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jsell on 27/3/2023.
+//
+
+#include "EMPLEADO.h"
---
+++ Pa/Lab/EMPLEADO.h
@@ -0,0 +1,16 @@
+//
+// Created by jsell on 27/3/2023.
+//
+
+#ifndef PA_EMPLEADO_H
+#define PA_EMPLEADO_H
+
+
+
+class EMPLEADO {
+
+};
+
+
+
+#endif //PA_EMPLEADO_H
